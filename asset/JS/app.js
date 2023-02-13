2787a9e081390cbf176a961e2facffa3d09837c5
khtrist <119412552+khtrist@users.noreply.github.com>
2023-02-14 00:47:27 +0400

    js1

---
+++ asset/JS/app.js
@@ -0,0 +1,14 @@
+
+var times = new Date();
+    var hours = times.getHours();
+   if (6 <= hours && hours < 16) {
+    document.body.style.backgroundColor = 'yellow'
+      alert ("Sabahin xeyr")
+    }
+    else (17 <= hours && hours < 23) ;{
+        document.body.style.backgroundColor = 'red'
+        alert ("Axshamin xeyr")
+    }
+   
+    document.getElementById("time").innerHTML = document.body ='ones';
+    
